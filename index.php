497a9365e00ad25787d15dbee1f9dd4b37e4280e
yvonnik <yvon.prevot@wanadoo.fr>
2022-04-03 18:27:24 +0200

    $IMAGE_SERVER en local pour Qnapsix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 //TODO : exemple de To do
-//$IMAGE_SERVER="/PhotoDb/"; // Machine locale au serveur Web, configuration standard en production
-$IMAGE_SERVER="http://localhost/PhotoDb/";
+$IMAGE_SERVER="/PhotoDb/"; // Machine locale au serveur Web, configuration standard en production
+//$IMAGE_SERVER="http://localhost/PhotoDb/";
 //$IMAGE_SERVER="https://192.168.2.11/PhotoDb/"; // Configuration de debug
 //$IMAGE_SERVER="http://localhost/PhotoDb/";
 include("dbconfig.php");
